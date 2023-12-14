8dde82d8ed98a3cb1e81d6b4253c44de4d5362cb
7decoeur <77decoeur@gmail.com>
2023-12-14 17:08:32 +0100

    Reliage
    J'ai relié le css et les images aux sites

--- App/Autocolants/autocolants.php
+++ App/Autocolants/autocolants.php
@@ -1,16 +1,15 @@
 <!doctype html>
 
 <head>
-    <link rel="stylesheet" href="../site.css">
+    <link rel="stylesheet" href="../../Public/site.css">
 </head>
 
 <body>
     <header id="haut">
-        <a href="../l'equipe/equipe.html" class="lien" id="eq">L'équipe</a>
+        <a href="../l'equipe/equipe.php" class="lien" id="eq">L'équipe</a>
         <a href="" class="lien">Nos actions</a>
-        <a href="../accueuil/site.html" class="lien"><img src="../images/logo_superpglue_inchala.webp"></a>
-        <a href="../Autocolants/autocolants.html" class="lien">Nos stickers</a>
+        <a href="../accueuil/site.php" class="lien"><img src="../../Public/images/logo_superpglue_inchala.webp"></a>
+        <a href="../Autocolants/autocolants.php" class="lien">Nos stickers</a>
         <a href="" class="lien" id="st">Nous soutenir</a>
     </header>
     <h1>NOS STICKERS</h1>
-    <?php foreach ($nom as $nom) : ?>
---
+++ App/l'equipe/equipe.php
@@ -0,0 +1,87 @@
+<!doctype html>
+
+<head>
+    <link rel="stylesheet" href="../../Public/site.css">
+</head>
+
+<body>
+    <header id="haut">
+        <a href="../l'equipe/equipe.php" class="lien" id="eq">L'équipe</a>
+        <a href="" class="lien">Nos actions</a>
+        <a href="../accueuil/site.php" class="lien"><img src="../../Public/images/logo_superpglue_inchala.webp"></a>
+        <a href="../Autocolants/autocolants.php" class="lien">Nos stickers</a>
+        <a href="" class="lien" id="st">Nous soutenir</a>
+    </header>
+    <img src="../../Public/images/grp.JPG" id="grp">
+    <h1>L'EQUIPE</h1>
+    <div id="grp_pht">
+        <a href="">
+            <div class="personne">
+                <img src="../../Public/images/alban.jpg">
+                <p>Alban MARTIN</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="personne">
+                <img src="../../Public/images/alexandre.jpg">
+                <p>Alexandre LEBON</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="personne">
+                <img src="../../Public/images/valentin.jpg">
+                <p>Valentin LAGANT</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="personne">
+                <img src="../../Public/images/lili.jpg">
+                <p>Lili-Rose BURGOS</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="personne">
+                <img src="../../Public/images/amina.jpg">
+                <p>AminaMEGHOUFEL</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="personne">
+                <img src="../../Public/images/alexis.jpg">
+                <p>Alexis TISON</p>
+            </div>
+        </a>
+        <a href="">
+            <div class="personne">
+                <img src="../../Public/images/ahinoa.jpg">
+                <p>Ahinoa RUIZ</p>
+            </div>
+        </a>
+    </div>
+    <p id="merci">Merci à tous nos amis scintillant, qui nous ont aidé ! Raphael, Liza, Eulalie.</p>
+    <a href="#haut"><img src="../../Public/images/icone-fleche-gauche-noir.png" id="fleche"></a>
+    <footer>
+        <div id="FL">
+            <p>Nous Contacter</p>
+            <ul>
+                <li><a href="">Nos actions</a></li>
+                <li><a href="../l'equipe/equipe.php">L'équipe</a></li>
+                <li><a href="../Autocolants/autocolants.php">Nos stickers</a></li>
+            </ul>
+        </div>
+        <div id="FC">
+            <p>Droits</p>
+            <ul>
+                <li><a href="../mentionslegales.html">Mentions légales</a></li>
+                <li><a href="">CGV</a></li>
+
+            </ul>
+        </div>
+        <div id="FR">
+            <p>Nos Réseaux</p>
+            <a href="https://discord.gg/jKGJEsTNAg"><img src="../../Public/images/logo-discord.png"></a>
+            <a href="https://www.instagram.com/wearesuperglue_/"><img src="../../Public/images/logo-instagram.png"></a>
+            <a href=""><img src="../../Public/images/logo-youtube.png"></a>
+            <a href="https://twitter.com/Superglu_e"><img src="../../Public/images/logo-X.png"></a>
+        </div>
+    </footer>
