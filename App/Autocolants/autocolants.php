b8beacf00394d5aeb49e8a0f5aaee0b3c39d3dce
Alban <alban77.martin@gmail.com>
2023-12-14 16:47:22 +0100

    superglue v1

---
+++ App/Autocolants/autocolants.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<head>
+    <link rel="stylesheet" href="../site.css">
+</head>
+
+<body>
+    <header id="haut">
+        <a href="../l'equipe/equipe.html" class="lien" id="eq">L'équipe</a>
+        <a href="" class="lien">Nos actions</a>
+        <a href="../accueuil/site.html" class="lien"><img src="../images/logo_superpglue_inchala.webp"></a>
+        <a href="../Autocolants/autocolants.html" class="lien">Nos stickers</a>
+        <a href="" class="lien" id="st">Nous soutenir</a>
+    </header>
+    <h1>NOS STICKERS</h1>
+    <?php foreach ($nom as $nom) : ?>
