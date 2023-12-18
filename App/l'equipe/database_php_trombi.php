97b9d1aaef0cf0bd6f61be15f65c64776e549592
aminamgf <ami.meghoufel@gmail.com>
2023-12-18 10:23:58 +0100

    page equipe

---
+++ App/l'equipe/database_php_trombi.php
@@ -0,0 +1,46 @@
+<?php
+
+$legroupe = [
+    1 => [
+        'firstname' => "Alban",
+        'lastname' => "Martin",
+        'photo' => 'alban.jpg',
+        'description' => "Chef de projet",
+    ],
+    2 => [
+        'firstname' => "Alexandre",
+        'lastname' => "Lebon",
+        'photo' => 'alexandre.jpg',
+        'description' => "Equipier polyvalent",
+    ],
+    3 => [
+        'firstname' => "Valentin",
+        'lastname' => "Lagant",
+        'photo' => 'valentin.jpg',
+        'description' => "Développeur web",
+    ],
+    4 => [
+        'firstname' => "Lili-Rose",
+        'lastname' => "Burgos",
+        'photo' => 'lili.jpg',
+        'description' => "Chargé de communication réseaux sociaux",
+    ],
+    5 => [
+        'firstname' => "Amina",
+        'lastname' => "Meghoufel",
+        'photo' => 'amina.jpg',
+        'description' => "Coordinatrice d'équipe",
+    ],
+    6 => [
+        'firstname' => "Alexis",
+        'lastname' => "Tison",
+        'photo' => 'alexis.jpg',
+        'description' => "Graphiste designer",
+    ],
+    7 => [
+        'firstname' => "Ahinoa",
+        'lastname' => "Ruiz",
+        'photo' => 'ahinoa.jpg',
+        'description' => "Photographe",
+    ],
+];
---
+++ App/l'equipe/fiche.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Trombinoscope - Aimé MIHI</title>
+
+
+    <link rel="stylesheet" type="text/css" href="trombi.css">
+</head>
+
+<body>
+
+    <header>
+
+   
+        <a href="equipe.php"><h1>Equipe </h1></a>
+        <p>Année 2023/2024</p>
+    </header>
+    <main>
+
+
+
+        <div class="fiche">
+            <div class="gauche">
+                <figure class="big_photo">
+                    <img src="./images/defaut.png" alt="photo par défaut">
+                </figure>
+            </div>
+            <div class="detail">
+                <p class="nom">  <span>Mihi</span></p>
+                <p class="ddn">01/02/2005</p>
+                <p class="groupe">groupe <span>12</span></p>
+                <p>
+                    Je suis un brillant élève de l'IUT Sénart Fontainebleau. Je suis passionné par le WEB et la création
+                    numérique.
+                </p>
+            </div>
+        </div>
+
+    </main>
+</body>
+
+</html>
