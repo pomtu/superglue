49c1e5048886c73a6fc9df767df442d32a927c07
7decoeur <77decoeur@gmail.com>
2023-12-18 08:59:59 +0100

    Mise a jour de la base de donnée pour les autocollants

--- App/Autocolants/Base_Donnée.php
+++ App/Autocolants/Base_Donnée.php
@@ -1,6 +1,10 @@
 <?php
 $sticker = [
-    1 => "LG⚡BT.webp",
-    2 => "Feu multicolor"
+    1 => "LG⚡BT",
+    2 => "Le feu de la fierté",
+    3 => "La bombe jeune et fun",
+    4 => "Le dé valide",
+    5 => "Le miroire de l'acceptation",
+    6 => "LE Superglue",
 ];
 ?>
