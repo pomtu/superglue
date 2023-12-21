22259fdd2dd7b91b21f3aaf9af37e4c21ce267a8
AlexisTison <tison.alexisjpm@gmail.com>
2023-12-21 15:20:28 +0100

    description 1.2

--- App/Autocolants/Base_Donnée_autocolant.php
+++ App/Autocolants/Base_Donnée_autocolant.php
@@ -2,13 +2,13 @@
 $sticker = [
     1 => [
         "nom" => "LG⚡BT",
-        "description"=>"Pour les fan de METAL et de paillettes"],
+        "description"=>"Pour les fans de Metal et de paillettes"],
     2 => [
         "nom" => "Feu Multicolore",
-        "description"=>"Ne vous arretez pas au genre"],
+        "description"=>"A chacun sa voie. N'attendez pas de passer au rose pour adotper ce sticker !"],
     3 => [
         "nom"=> "Dynamite",
-        "description"=>"Une explosion de pailettes"],
+        "description"=>"Une explosion de pailettes dans votre quotidien !!!"],
     4 => [
         "nom" => "L.G.B.Dé",
         "description"=>"Parceque le genre n'est pas un choix"],
@@ -17,6 +17,6 @@ $sticker = [
         "description"=>"quand la vie vous blaisse, on vous soigne"],
     6 => [
         "nom" => "LE Superglue",
-        "description"=>"L'autocollant du meilleur des groupes"],
+        "description"=>"Marquez votre appartenance au meilleur des groupes"],
 ];
 ?>
