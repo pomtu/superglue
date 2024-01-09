f33c8bb24bbc80c1968cf234f1cf167a27500814
7decoeur <77decoeur@gmail.com>
2024-01-09 09:00:33 +0100

    Update header

--- App/view/Common/header.php
+++ App/view/Common/header.php
@@ -7,10 +7,10 @@
 <body>
     <header id="haut">
         <ul>
-            <li class="ListeHeader"><a href="../L'équipe/equipe.php" class="lien" id="eq">L'équipe</a></li>
-            <li class="ListeHeader"><a href="" class="lien">Nos actions</a></li>
-            <li class="ListeHeader"><a href="../Accueil/site.php" class="lien"><img src="../../../Public/images/logo_superpglue_inchala.webp"></a></li>
+            <li class="ListeHeader"><a href="" class="lien" id="eq">Nos actions</a></li>
             <li class="ListeHeader"><a href="../Autocollants/autocollants.php" class="lien">Nos stickers</a></li>
+            <li class="ListeHeader"><a href="../Accueil/site.php" class="lien"><img src="../../../Public/images/logo_superpglue_inchala.webp"></a></li>
+            <li class="ListeHeader"><a href="../L'équipe/equipe.php" class="lien">L'équipe</a></li>
             <li class="ListeHeader"><a href="" class="lien" id="st">Nous soutenir</a></li>
         </ul>
     </header>
