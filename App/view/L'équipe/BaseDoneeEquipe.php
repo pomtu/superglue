7e866e3b87f8c20aab16d4fd011999a196b6e7c8
Alban <alban77.martin@gmail.com>
2024-01-19 14:45:37 +0100

    Update BaseDoneeEquipe.php

--- App/view/L'équipe/BaseDoneeEquipe.php
+++ App/view/L'équipe/BaseDoneeEquipe.php
@@ -6,16 +6,19 @@ $legroupe = [
         "nom"=>"Alban",
         "role"=>"Chef de projet",
         "age"=>"18",
-        "description"=>"",
-        "aime"=>""
+        "description"=>"Ma chanson préféré est True love will find you in the end (@oxywantsmore sur IG)((Ajoutez moi par pitié <3))
+        ",
+        "aime"=>"La musique (en faire et en écouter),les tatouages, la techno (je tape du pied), les chats (miaou), les vinyles, l’amour, l’escalade, les drapeaux, le design, la mode et le bibimbap végé de mon resto coréen préféré(dans le 3e). 
+        "
     ],
     2=>[
         "photo"=>"alexandre.jpg",
         "nom"=>"Alexandre",
         "role"=>"Equipier polyvalent",
         "age"=>"18",
-        "description"=>"",
-        "aime"=>"",
+        "description"=>"Connaissez-vous Sleep Token ?",
+        "aime"=>"SLEEP puis TOKEN puis SLEEP puis TOKEN puis SLEEP puis TOKEN puis SLEEP puis TOKEN puis SLEEP puis TOKEN puis SLEEP et enfin TOKEN
+        ",
     ],
     3=>[
         "photo"=>"valentin.jpg",
@@ -30,16 +33,20 @@ $legroupe = [
         "nom"=>"Amina",
         "role"=>"Coordinatrice d’équipe",
         "age"=>"18 ans",
-        "description"=>"",
-        "aime"=>""
+        "description"=>"Je suis une personne qui n'arrive pas à écrire sa description.
+        ",
+        "aime"=>"on A jugé mes passions (reLou.e.s), donc Bon, j’ai décidé de ne plus les partager. merci..… tu sAis qui tu es. je Ne dirais pas qui c’est parce qu’il va se fâcher.
+        "
     ],
     5=>[
         "photo"=>"lili.jpg",
         "nom"=>"Lili-Rose",
         "role"=>"Chargée de communication sur les réseaux sociaux",
         "age"=>"17",
-        "description"=>"",
-        "aime"=>""
+        "description"=>"Étudiante en BUT MMI, j’habite l’Ile-de-France, et je m’occupe de la communication de Superglue sur les différents réseaux sociaux. 
+        Je suis l’élève très sérieuse de ce groupe, et je suis celle qui crampte le plus, voilà pourquoi j’ai les meilleures apagnan du groupe.
+        ",
+        "aime"=>"J’aime beaucoup beaucoup beaucoup les chats. Mes musiques préférées sont Néons Rouges/ Belles chansons de Luidji et Yebba’s Heartbreak de Yebba. Les rubans et les nœuds ainsi que les figurines pop mart sont mes obsessions du moment. Mon film préféré est Le Château ambulant de Miyazaki. BISOUS"
     ],
     
     6=>[
@@ -47,16 +54,21 @@ $legroupe = [
         "nom"=>"Alexis",
         "role"=>"Graphiste",
         "age"=>"23",
-        "description"=>"",
-        "aime"=>""
+        "description"=>"Je suis un étudiant en BUT MMI en manque d'inspi 
+        ",
+        "aime"=>"Dessiner, les cartes et sûrement d’autres trucs    
+        "
     ],
     7=>[
         "photo"=>"ahinoa.jpg",
         "nom"=>"Ahinoa",
         "role"=>"Photographe",
         "age"=>"19",
-        "description"=>"",
-        "aime"=>""
+        "description"=>"*bruit de cigales*
+        Hola ! Je m’appelle Ahinoa, j’ai 19 ang et je viens du Sud. Étant plutôt quelqu’ung de créatif, j’aime tout ce qui tourne autour de la pétanque et du ricard. Retrouvez moi à la couleur de la culotte ou bieng chez Tonton du lundi au lundi, de 00h à 00h (j’y habite). Besitos sur la fesse droite. Ou la gauche, selon vos préférences. Et toi, tu tires ou tu pointes minot ?
+       ",
+        "aime"=>"Le saucissong, le ricard, le tuning, les marcels,les clopes, Jacky (surtout son saucisson), et Michel les jeudis.
+        "
     ],
 ];
 ?>
