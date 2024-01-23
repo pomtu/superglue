ec0c293c91660733e25c22184ae70ef701583c65
7decoeur <77decoeur@gmail.com>
2024-01-23 15:40:19 +0100

    centrage video

--- App/view/Action/Action.php
+++ App/view/Action/Action.php
@@ -22,7 +22,7 @@
    </div>
     <
     <?php include("Carrousel.html") ?>
-    <div id=vidéo>
+    <div id=video>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/J6loDQM1d14?si=8ZBWmIOwViH51uyr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     
