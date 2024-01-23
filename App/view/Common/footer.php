077e479fa54910d2268a56d0e5e2d9fd13d32ac4
aminamgf <ami.meghoufel@gmail.com>
2024-01-23 14:20:26 +0100

    modif footer

--- App/view/Common/footer.php
+++ App/view/Common/footer.php
@@ -2,14 +2,14 @@
 <a href="#haut"><img src="../../../Public/images/icone-fleche-gauche-noir.png" id="fleche"></a>
     <footer>
         <div id="FL">
-            <p>NOUS CONTACTER</p>
+            <p>Nous contacter</p>
             <ul class="LiFooter">
-                <li><a href="" class="TxtFooter">Nos coordonées</a></li>
-                <li><a href="" class="TxtFooter">Formulaire de contact</a></li>
+            <li><a href="" class="TxtFooter">Formulaire de contact</a></li>
+            <li><a href="" class="TxtFooter">Nos coordonées</a></li>
             </ul>
         </div>
         <div id="FC">
-            <p>DROITS</p>
+            <p>Droits</p>
             <ul class="LiFooter">
                 <li><a href="../Common/mentionslegales.html" class="TxtFooter">Mentions légales</a></li>
                 <li><a href="" class="TxtFooter">CGV</a></li>
@@ -17,7 +17,7 @@
             </ul>
         </div>
         <div id="FR">
-            <p>NOS RÉSAUX</p>
+            <p>Nos réseaux</p>
             <a href="<?=$reseauxsociaux['1']['lien']?>"><img src="<?=$reseauxsociaux['1']['photo'] ?>" alt="<?=$reseauxsociaux['1']['nom'] ?>"></a>
             <a href="<?=$reseauxsociaux['2']['lien']?>"><img src="<?=$reseauxsociaux['2']['photo'] ?>" alt="<?=$reseauxsociaux['2']['nom'] ?>"></a>
             <a href="<?=$reseauxsociaux['3']['lien']?>"><img src="<?=$reseauxsociaux['3']['photo'] ?>" alt="<?=$reseauxsociaux['3']['nom'] ?>"></a>
