11724ff985c752ae5fbddd230b345ad167603913
7decoeur <77decoeur@gmail.com>
2024-01-23 14:41:11 +0100

    rectifications

--- App/view/Common/header.php
+++ App/view/Common/header.php
@@ -9,7 +9,7 @@
         <ul>
             <li class="ListeHeader"><a href="../Action/Action.php" class="lien" id="eq">Nos actions</a></li>
             <li class="ListeHeader"><a href="../Autocollants/autocollants.php" class="lien">Nos stickers</a></li>
-            <li class="ListeHeader"><a href="../Accueil/site.php" class="lien"><img src="../../../Public/images/logo_superpglue_inchala.webp"></a></li>
+            <li class="ListeHeader"><a href="../Accueil/site.php" class="lien"><img src="../../../Public/images/logo_superpglue_inchala.webp" id='logoHeader'></a></li>
             <li class="ListeHeader"><a href="../L'équipe/equipe.php" class="lien">L'équipe</a></li>
             <li class="ListeHeader"><a href="" class="lien" id="st">Nous soutenir</a></li>
         </ul>
